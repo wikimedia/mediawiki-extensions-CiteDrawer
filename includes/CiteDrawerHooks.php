53710bbb5dc26513b587da5ecc5c7cfdcf890d0f
Vedmaka <god.vedmaka@gmail.com>
2023-09-14 22:04:43 +0400

    1.39+ compatibility
    Change-Id: I6690f25fb8fcf2efd90b029d83fc9c33b98d656a

--- includes/CiteDrawerHooks.php
+++ includes/CiteDrawerHooks.php
@@ -1,29 +1,38 @@
 <?php
 
+namespace Mediawiki\Extensions\CiteDrawer;
+
+use ConfigFactory;
+use MediaWiki\Hook\BeforePageDisplayHook;
+use MediaWiki\Hook\MakeGlobalVariablesScriptHook;
+
 /**
  * Hooks for CiteDrawer extension
  *
  * @file
  * @ingroup Extensions
  */
-class CiteDrawerHooks {
+class CiteDrawerHooks implements BeforePageDisplayHook, MakeGlobalVariablesScriptHook {
 
-	/**
-	 * @param OutputPage $out
-	 * @param Skin $skin
-	 */
-	public static function onBeforePageDisplay( $out, $skin ) {
+	/** @var ConfigFactory */
+	private $configFactory;
+
+	/** @inheritDoc */
+	public function __construct( ConfigFactory $configFactory ) {
+		$this->configFactory = $configFactory;
+	}
+
+	/** @inheritDoc */
+	public function onBeforePageDisplay( $out, $skin ): void {
 		$out->addModules( 'ext.citedrawer.main' );
 	}
 
-	/**
-	 * @param array &$vars
-	 * @param OutputPage $out
-	 */
-	public static function onMakeGlobalVariablesScript( &$vars, $out ) {
-		$vars['wgCiteDrawerEnableDesktop'] = $out->getConfig()->get( 'CiteDrawerEnableDesktop' );
-		$vars['wgCiteDrawerEnableMobile'] = $out->getConfig()->get( 'CiteDrawerEnableMobile' );
-		$vars['wgCiteDrawerTheme'] = $out->getConfig()->get( 'CiteDrawerTheme' );
+	/** @inheritDoc */
+	public function onMakeGlobalVariablesScript( &$vars, $out ): void {
+		$config = $this->configFactory->makeConfig( 'CiteDrawer' );
+		$vars['wgCiteDrawerEnableDesktop'] = $config->get( 'CiteDrawerEnableDesktop' );
+		$vars['wgCiteDrawerEnableMobile'] = $config->get( 'CiteDrawerEnableMobile' );
+		$vars['wgCiteDrawerTheme'] = $config->get( 'CiteDrawerTheme' );
 	}
 
 }
